03f580e8058591b879568983c6f085ea4a502618
Esteban Gonzalez <e-gonzalez@live.ca>
2022-01-21 11:50:30 -0700

    second commit

--- src/js/read.js
+++ src/js/read.js
@@ -1,1 +1,36 @@
- 
+/*
+        Read First
+
+*/
+
+import {ref, get} from 'firebase/database';
+import {db} from './libs/firebase/firebaseConfig'; 
+
+// get the data load in session storage... update delete
+
+let store = []
+async function pageInit(){
+    const userRef = ref(db, "users/")
+    const userSnapShot = await get(userRef)
+    const userData = userSnapShot.val()
+    console.log(userData)
+    const users = Object.keys(userData).map(key=>{
+        userData[key].id = key;
+        return userData[key]
+    })
+    // build out the user card component
+    // html + css ui/view userCard
+    //template
+
+    // rendering    component + data ===> ui
+
+    console.log(users)
+    // object
+    // object.property
+    // object['property']
+
+
+    //sessionStorage.setItem('store')
+}
+
+pageInit()
